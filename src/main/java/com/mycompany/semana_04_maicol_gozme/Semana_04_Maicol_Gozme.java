1cd435fdc1ca8cc4acb8c4897ddd54b23e3ff8e6
miguel <miguel@zack>
2024-10-05 21:26:31 -0500

    Semana04

---
+++ src/main/java/com/mycompany/semana_04_maicol_gozme/Semana_04_Maicol_Gozme.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana_04_maicol_gozme;
+
+import Vista.Registro;
+
+/**
+ *
+ * @author miguel
+ */
+public class Semana_04_Maicol_Gozme {
+
+    public static void main(String[] args) {
+             try {
+            // Set HiFi Look and Feel from JTattoo
+              javax.swing.UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+} catch (ClassNotFoundException ex) {
+    java.util.logging.Logger.getLogger(Semana_04_Maicol_Gozme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} catch (InstantiationException ex) {
+    java.util.logging.Logger.getLogger(Semana_04_Maicol_Gozme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} catch (IllegalAccessException ex) {
+    java.util.logging.Logger.getLogger(Semana_04_Maicol_Gozme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+    java.util.logging.Logger.getLogger(Semana_04_Maicol_Gozme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+}
+
+         java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new Registro().setVisible(true);
+        }
+    });
+    }
+}
